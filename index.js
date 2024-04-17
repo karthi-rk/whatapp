8f885edc2826ebc706a6ecc5681b1643c4924663
ajiths <ajiths@agilecyber.com>
2024-04-17 12:19:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 80;
-
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World from Express!');
 });
