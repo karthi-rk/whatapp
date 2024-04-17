5571ac3216fef57889fb54f903153faf0adf093c
ajiths <ajiths@agilecyber.com>
2024-04-17 17:30:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,133 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World from Express!');
 });
+pet = { "918220179756": ["Max", "Julie"], "other": ["Doggy", "Puppy"] };
 
+function getinteractive_service() {
+    return {
+        "messaging_product": "whatsapp",
+        "to": req.body.entry[0].changes[0].value.messages[0].from,
+        "type": "interactive",
+
+        "interactive": {
+            "type": "list",
+            "header": {
+                "type": "text",
+                "text": "Please choose service"
+            },
+            "body": {
+                "text": "You can choose only one service at a time!"
+            },
+            "action": {
+                "button": "Choose",
+                "sections": [
+                    {
+                        "title": "Services",
+                        "rows": [
+                            {
+                                "id": "service-1",
+                                "title": "Dog Walking",
+                                "description": "Explore the City"
+                            },
+                            {
+                                "id": "service-2",
+                                "title": "Day Care",
+                                "description": "Experience the rich history"
+                            },
+                        ]
+                    },
+
+                ]
+            }
+        }
+    }
+};
+function getpetInteractiveJson(req) {
+
+    return {
+        "messaging_product": "whatsapp",
+        "to": req.body.entry[0].changes[0].value.messages[0].from,
+        "type": "interactive",
+
+        "interactive": {
+            "type": "list",
+            "header": {
+                "type": "text",
+                "text": "Please choose your pet"
+            },
+            "body": {
+                "text": "You can choose only one pet at a time"
+            },
+            "action": {
+                "button": "Choose",
+                "sections": [
+                    {
+                        "title": "Pet",
+                        "rows": [
+                            {
+                                "id": "pet-1",
+                                "title": "Max",
+                                "description": ""
+                            },
+                            {
+                                "id": "pet-2",
+                                "title": "Puppy",
+                                "description": ""
+                            },
+                        ]
+                    },
+
+                ]
+            }
+        }
+
+    }
+};
+function getslotInteractiveJson() {
+    return {
+        "messaging_product": "whatsapp",
+        "to": req.body.entry[0].changes[0].value.messages[0].from,
+        "type": "interactive",
+
+        "interactive": {
+            "type": "list",
+            "header": {
+                "type": "text",
+                "text": "Please choose your slot"
+            },
+            "body": {
+                "text": "You can choose only one slot at a time"
+            },
+            "action": {
+                "button": "Choose",
+                "sections": [
+                    {
+                        "title": "Slot",
+                        "rows": [
+                            {
+                                "id": "slot-1",
+                                "title": "Morning",
+                                "description": ""
+                            },
+                            {
+                                "id": "slot-2",
+                                "title": "Afternoon",
+                                "description": ""
+                            },
+                            {
+                                "id": "slot-3",
+                                "title": "Evening",
+                                "description": ""
+                            },
+                        ]
+                    },
+
+                ]
+            }
+        }
+
+    }
+}
 app.get('/receive-message', (req, res) => {
     console.log("sample inside the receive message")
     console.log(req.body);
@@ -18,10 +144,17 @@ app.get('/receive-message', (req, res) => {
 app.post('/receive-message', (req, res) => {
     console.log(JSON.stringify(req.body.entry[0].changes))
     if (req.body.entry[0].changes[0].value.messages)
-        sendWhatsappMessage();
+        sendWhatsappMessage(req);
     res.send(req.query['hub.challenge']);
 });
-function sendWhatsappMessage() {
+function getTemplateFromInteractiveMessage(interactive) {
+    if (interactive.list_reply.id.includes("service")) {
+        return getpetInteractiveJson(req);
+    } else if (interactive.list_reply.id.includes("slot")) {
+        return getslotInteractiveJson(req);
+    }
+}
+function sendWhatsappMessage(req) {
     var request = require('request');
     var options = {
         'method': 'POST',
@@ -30,71 +163,7 @@ function sendWhatsappMessage() {
             'Authorization': 'Bearer EAAPsguiCMzMBOyVbZCFGr1TO9fEbVzp1AWvLyKQAofmBjMp9g702UOtcuUZAWZBZCv683IR9T9FneBLg5vZAkZAYpq97xhvxQA9Jw68uSy9b24yVRlPqzrHryMLbasnBfZAbgGbp2nwhZBAZCZCyHjyoDZBdoryUn2e164zYBddverujO566gs8n4FeuskYWSZCZAcMHobOyXHj9bYcckcX1ww3AZD',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            "messaging_product": "whatsapp",
-            "to": "7012823508",
-            "type": "interactive",
-
-            "interactive": {
-                "type": "list",
-                "header": {
-                    "type": "text",
-                    "text": "Top Destinations"
-                },
-                "body": {
-                    "text": "Check out these popular travel destinations!"
-                },
-                "footer": {
-                    "text": "Book your trip today!"
-                },
-                "action": {
-                    "button": "View Options",
-                    "sections": [
-                        {
-                            "title": "Europe",
-                            "rows": [
-                                {
-                                    "id": "paris",
-                                    "title": "Paris, France",
-                                    "description": "Explore the City"
-                                },
-                                {
-                                    "id": "london",
-                                    "title": "London, England",
-                                    "description": "Experience the rich history"
-                                },
-                                {
-                                    "id": "barcelona",
-                                    "title": "Barcelona, Spain",
-                                    "description": "Immerse yourself"
-                                }
-                            ]
-                        },
-                        {
-                            "title": "Asia",
-                            "rows": [
-                                {
-                                    "id": "tokyo",
-                                    "title": "Tokyo, Japan",
-                                    "description": "Discover"
-                                },
-                                {
-                                    "id": "bali",
-                                    "title": "Bali, Indonesia",
-                                    "description": "Relax "
-                                },
-                                {
-                                    "id": "bangkok",
-                                    "title": "Bangkok, Thailand",
-                                    "description": "Immerse yourself"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-
-        })
+        body: req.body.entry[0].changes[0].value.messages[0].type != "interactive" ? JSON.stringify(getinteractive_service(req)) : getTemplateFromInteractiveMessage(req.body.entry[0].changes[0].value.messages[0].interactive)
 
     };
     request(options, function (error, response) {
